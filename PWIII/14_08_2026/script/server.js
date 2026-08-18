136c8efae36ccb2e4bb5126fa9e41393b429defd
Discente <Discente@etec224.edu.br>
2026-08-18 18:29:39 -0300

    New Api of weather

--- PWIII/14_08_2026/script/server.js
+++ PWIII/14_08_2026/script/server.js
@@ -2,7 +2,7 @@ const btnBuscar = document.getElementById("buscar");
 
 btnBuscar.addEventListener('click', buscarCep);
 
-async function buscarCep(){
+async function buscarCep() {
     // Inputs 
     const cepInput = document.getElementById("cep");
     const cep = cepInput.value;
@@ -10,15 +10,19 @@ async function buscarCep(){
     // Resultados HTML'S
     const resultado = document.getElementById("resultado");
 
-    // Requisição API
-    const reqApi = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-    const json = await reqApi.json();
+    try {
+        // Requisição API
+        const reqApi = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        const json = await reqApi.json();
 
-    resultado.innerHTML = `
+        resultado.innerHTML = `
         <p><strong>CEP:</strong> ${json.cep}<br></p>
         <p><strong>LOGRADOURO:</strong> ${json.logradouro}<br></p>
         <p><strong>BAIRRO:</strong> ${json.bairro}<br></p>
         <p>UF: ${json.uf}</p>
         <p><strong>ESTADO:</strong> ${json.estado}</p>
     `
+    } catch (err) {
+        resultado.innerHTML = `[ERRO] ao buscar a requisição do endpoint!`
+    }
 }
---
+++ PWIII/18_08_2026/scripts/script.js
@@ -0,0 +1,58 @@
+const typeWeather = document.getElementById('searchWeather');
+
+async function queryWeather(){
+
+    const weatherValue = typeWeather.value;
+    // Mostra o resultado da api que irá aparecer no navegador.
+    const resultado = document.querySelector('.resultado');
+    try{
+        // Busca os dados do endpoint através da cidade solicitada pelo usuário
+        const fetchData = await fetch(`
+            https://geocoding-api.open-meteo.com/v1/search?name=${weatherValue}`
+        );
+        // transforma os dados da API no formato json
+        const weatherJson = await fetchData.json();
+
+        // Descobri a latitude e logitude.
+        const latidude = weatherJson.results[0].latitude;
+        const longitude = weatherJson.results[0].longitude;
+
+        // Descobrir os dados de clima atráves da latidude e longitude
+        const meteoFetch = await fetch(`
+            https://api.open-meteo.com/v1/forecast?latitude=${latidude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,apparent_temperature,wind_speed_10m,wind_direction_10m
+
+        `);
+        // transforma os dados da API no formato json
+        const responseMeteo = await meteoFetch.json();
+
+        // Obtendo os dados do objeto
+        const pais = weatherJson.results[0].country;
+        const estado = weatherJson.results[0].admin1;
+        const cidade = weatherJson.results[0].name;
+        const temperatura = responseMeteo.current.temperature_2m;
+        const sensacao_termica = responseMeteo.current.apparent_temperature;
+        const umidade = responseMeteo.current.relative_humidity_2m;
+        const vento = responseMeteo.current.wind_speed_10m;
+
+        
+        // Mostra no navegador os dados gerados pela API
+        resultado.innerHTML = `
+            <ul>
+                <li>Cidade: ${estado}</li>
+                <li>País: ${pais}</li>
+                <li>Temperatura: ${temperatura}°C</li>
+                <li>Sensação Térmica: ${sensacao_termica}°C</li>
+                <li>Umidade: ${umidade}%</li>
+                <li>Vento: ${vento}</li>
+            </ul>
+        `;
+
+        // Limpa o input
+        typeWeather.value = "";
+
+    } catch(err) {
+        // Esse bloco tem a função onde caso algum erro na api o código pula para esse bloco
+        console.log(`Erro!`);
+    }
+
+}
